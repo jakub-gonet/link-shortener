2d6744f64e52ad9d2ff6b3014f90b75ad2f56101
Jakub Gonet <jakub.gonet0@gmail.com>
2019-03-27 21:29:37 +0100

    SController: Implement show and remove index stub

--- app/controllers/s_controller.rb
+++ app/controllers/s_controller.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class SController < ApplicationController
-  def index; end
+  def show
+    url = Url.find_by shortened_url: params[:id]
+    if !url.nil?
+      redirect_to url.base_url
+    else
+      redirect_to root_path, alert: 'Requested URL not found'
+    end
+  end
 end
