9ad605cb0402701481686d523a2401a74eb3cab9
Jakub Gonet <jakub.gonet0@gmail.com>
2019-03-27 08:22:30 +0100

    Url: Add method returning error messages

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,8 @@
 class Url < ApplicationRecord
   validates :base_url, presence: true
   validates :shortened_url, presence: true
+
+  def error_messages
+    errors.full_messages.join('\n')
+  end
 end
