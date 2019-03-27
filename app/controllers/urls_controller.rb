c567386740f6dddb286024dda412b5406ffa585c
Jakub Gonet <jakub.gonet0@gmail.com>
2019-03-27 08:30:33 +0100

    UrlsController: Implement create

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -5,4 +5,17 @@ class UrlsController < ApplicationController
   def show
     @url = Url.find(params[:id])
   end
+
+  def create
+    base_url = params[:url][:base_url]
+    @url = Url.find_or_create_by(base_url: base_url) do |url|
+      url.shortened_url = ShortenedLinksGenerator.shorten(url)
+    end
+
+    if @url.save
+      redirect_to @url
+    else
+      redirect_to root_path, alert: @url.error_messages
+    end
+  end
 end
