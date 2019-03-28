3fd4b9c345a08fe3dde57b83caffd696e4393a29
Jakub Gonet <jakub.gonet0@gmail.com>
2019-03-28 17:24:54 +0100

    UrlModel: Add checker for domain validity

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+require 'addressable/uri'
+
 class Url < ApplicationRecord
   validates :base_url, presence: true
   validates :shortened_url, presence: true, uniqueness: true
@@ -5,4 +9,16 @@ class Url < ApplicationRecord
   def error_messages
     errors.full_messages.join('\n')
   end
+
+  class << self
+    def domain_forbidden?(domain)
+      return true if domain.nil?
+
+      forbidden = Rails.application.config.forbidden_domains
+      forbidden.all? do |x|
+        !domain.start_with?(x)
+      end
+    end
+  end
+
 end
