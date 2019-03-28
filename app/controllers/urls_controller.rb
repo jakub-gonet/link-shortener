b68b473e5dd0738d0b109534d85004d52162b9ff
Jakub Gonet <jakub.gonet0@gmail.com>
2019-03-28 19:07:00 +0100

    UrlController: Add default value for base url and fix block

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
 class UrlsController < ApplicationController
-
   def show
     @url = Url.find(params[:id])
   end
 
   def create
-    base_url = params[:url][:base_url]
+    base_url = params[:url][:base_url] || ''
     @url = Url.find_or_create_by(base_url: base_url) do |url|
-      url.shortened_url = ShortenedLinksGenerator.shorten(url)
+      url.shortened_url = ShortenedLinksGenerator.shorten(base_url)
     end
     if @url.valid?
       redirect_to @url
