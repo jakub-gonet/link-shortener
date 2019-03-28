291f2aae9b653af40a1d26f3854f88202d08d669
Jakub Gonet <jakub.gonet0@gmail.com>
2019-03-28 07:54:06 +0100

    UrlModel: Add uniquess

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,6 @@
 class Url < ApplicationRecord
   validates :base_url, presence: true
-  validates :shortened_url, presence: true
+  validates :shortened_url, presence: true, uniqueness: true
 
   def error_messages
     errors.full_messages.join('\n')
