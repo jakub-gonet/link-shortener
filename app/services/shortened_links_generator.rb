c44237d96784319ae53871dc7471346c55b69667
Jakub Gonet <jakub.gonet0@gmail.com>
2019-03-28 18:22:48 +0100

    ShortenedLinkGen: Add methods for reading files with words

--- app/services/shortened_links_generator.rb
+++ app/services/shortened_links_generator.rb
@@ -3,5 +3,24 @@ module ShortenedLinksGenerator
 
   def shorten(url)
     nil
+
+  def resource_from_file(path)
+    res = []
+    File.open(path) do |f|
+      f.each_line do |line|
+        res.push(line.strip)
+      end
+    end
+    res
+  end
+
+  def self.adjectives
+    file = File.join(File.dirname(__FILE__), 'adjectives.txt')
+    @ADJECTIVES ||= resource_from_file(file)
+  end
+
+  def self.animals
+    file = File.join(File.dirname(__FILE__), 'animals.txt')
+    @ANIMALS ||= resource_from_file(file)
   end
 end
