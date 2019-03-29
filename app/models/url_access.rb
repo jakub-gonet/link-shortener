eeb651e06118bb6827a91375e39ff3427d65f397
Jakub Gonet <jakub.gonet0@gmail.com>
2019-03-29 11:01:29 +0100

    UrlAccess: Implement url_views_since() and url_views_by_ip methods

--- app/models/url_access.rb
+++ app/models/url_access.rb
@@ -1,4 +1,17 @@
+# frozen_string_literal: true
+
 class UrlAccess < ApplicationRecord
   belongs_to :url
   validates :ip, presence: true
+
+  def self.url_views_since(shortened, date)
+    UrlAccess.joins(:url)
+             .where('shortened_url = :url AND url_accesses.created_at >= :date',
+                    url: shortened, date: date)
+             .all
+  end
+
+  def self.url_views_by_ip(shortened, ip, date)
+    url_views_since(shortened, date).where("ip = #{ip}").all
+  end
 end
