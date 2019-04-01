7630cb9ab9694f7cef75fb87b8aa42c5e43ce463
Jakub Gonet <jakub.gonet0@gmail.com>
2019-04-01 21:47:06 +0200

    UrlAccessModelTest: Change before(:each) to let()

--- spec/models/url_access_spec.rb
+++ spec/models/url_access_spec.rb
@@ -8,15 +8,14 @@ VALID_IP = '127.0.0.1'
 RSpec.describe UrlAccess, type: :model do
   describe 'validation' do
     context 'with valid url model' do
-      before(:each) do
-        @url = Url.create!(base_url: VALID_URL)
-      end
+      let(:url) { Url.create!(base_url: VALID_URL) }
+
       it 'is valid when IP address is provided' do
-        url_access = @url.url_accesses.create(ip: VALID_IP)
+        url_access = url.url_accesses.create(ip: VALID_IP)
         expect(url_access).to be_valid
       end
       it 'is invalid when IP address is not provided' do
-        url_access = @url.url_accesses.create(ip: nil)
+        url_access = url.url_accesses.create(ip: nil)
         expect(url_access).to_not be_valid
       end
     end
