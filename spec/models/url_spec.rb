412b7d58a417f81829762fc256905e8960478540
Jakub Gonet <jakub.gonet0@gmail.com>
2019-04-01 19:18:45 +0200

    UrlModelTests: Add 'validation' describe block

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -8,33 +8,35 @@ VALID_URLS = ['https://youtu.be/dQw4w9WgXcQ',
 URL_FROM_INVALID_DOMAIN = 'http://bit.do/Sans'
 INVALID_URLS = ['', 'badziebabla'].freeze
 
-RSpec.describe Url, type: :model do
-  VALID_URLS.each do |valid_url|
-    it 'is valid with properly structured url' do
-      url = Url.create(base_url: valid_url)
-      expect(url).to be_valid
+RSpec.describe(Url, type: :model) do
+  describe 'validation' do
+    VALID_URLS.each do |valid_url|
+      it 'is valid when properly structured url' do
+        url = Url.create(base_url: valid_url)
+        expect(url).to be_valid
+      end
     end
-  end
 
-  it 'is invalid with url from forbidden domains' do
-    url = Url.create(base_url: URL_FROM_INVALID_DOMAIN)
-    expect(url).to_not be_valid
-  end
-
-  it 'is invalid with nil as url' do
-    url = Url.create(base_url: nil)
-    expect(url).to_not be_valid
-  end
-
-  it 'is invalid with empty string as url' do
-    url = Url.create(base_url: '')
-    expect(url).to_not be_valid
-  end
-
-  INVALID_URLS.each do |invalid_url|
-    it 'is invalid with invalid structured url' do
-      url = Url.create(base_url: invalid_url)
+    it 'is invalid when url from forbidden domains' do
+      url = Url.create(base_url: URL_FROM_INVALID_DOMAIN)
       expect(url).to_not be_valid
+    end
+
+    it 'is invalid when nil as url' do
+      url = Url.create(base_url: nil)
+      expect(url).to_not be_valid
+    end
+
+    it 'is invalid when empty string as url' do
+      url = Url.create(base_url: '')
+      expect(url).to_not be_valid
+    end
+
+    INVALID_URLS.each do |invalid_url|
+      it 'is invalid when invalid structured url' do
+        url = Url.create(base_url: invalid_url)
+        expect(url).to_not be_valid
+      end
     end
   end
 end
