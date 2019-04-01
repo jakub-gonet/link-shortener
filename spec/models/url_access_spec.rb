0c80b3466fe8e8ac36f221406f884da7b23e834a
Jakub Gonet <jakub.gonet0@gmail.com>
2019-04-01 21:41:17 +0200

    UrlAccessModelTest: Implement validation tests

--- spec/models/url_access_spec.rb
+++ spec/models/url_access_spec.rb
@@ -1,5 +1,24 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
+VALID_URL = 'https://youtu.be/dQw4w9WgXcQ'
+VALID_IP = '127.0.0.1'
+
 RSpec.describe UrlAccess, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    context 'with valid url model' do
+      before(:each) do
+        @url = Url.create!(base_url: VALID_URL)
+      end
+      it 'is valid when IP address is provided' do
+        url_access = @url.url_accesses.create(ip: VALID_IP)
+        expect(url_access).to be_valid
+      end
+      it 'is invalid when IP address is not provided' do
+        url_access = @url.url_accesses.create(ip: nil)
+        expect(url_access).to_not be_valid
+      end
+    end
+  end
 end
