81973c7b295746929bad64a00ba0194e53fd1ad4
Jakub Gonet <jakub.gonet0@gmail.com>
2019-04-01 22:49:32 +0200

    ShortenedLinkGen: Change module into class with instance methods

--- app/lib/shortened_links_generator.rb
+++ app/lib/shortened_links_generator.rb
@@ -1,9 +1,7 @@
-module ShortenedLinksGenerator
+class ShortenedLinksGenerator
   require 'digest/sha1'
 
-  module_function
-
-  def shorten(url, adj_n = 2, animals_n = 1)
+  def self.shorten(url, adj_n = 2, animals_n = 1)
     hash = Digest::SHA1.hexdigest(url).to_i(16)
     rng = Random.new(hash)
     adjectives_s = adjectives.sample(adj_n, random: rng).join('-')
@@ -11,25 +9,27 @@ module ShortenedLinksGenerator
     "#{adjectives_s}-#{animals_s}"
   end
 
-  private
+  class << self
+    private
 
-  def resource_from_file(path)
-    res = []
-    File.open(path) do |f|
-      f.each_line do |line|
-        res.push(line.strip)
+    def resource_from_file(path)
+      res = []
+      File.open(path) do |f|
+        f.each_line do |line|
+          res.push(line.strip)
+        end
       end
+      res
     end
-    res
-  end
 
-  def adjectives
-    file = File.join(File.dirname(__FILE__), 'adjectives.txt')
-    @adjectives ||= resource_from_file(file)
-  end
+    def adjectives
+      file = File.join(File.dirname(__FILE__), 'adjectives.txt')
+      @adjectives ||= resource_from_file(file)
+    end
 
-  def animals
-    file = File.join(File.dirname(__FILE__), 'animals.txt')
-    @animals ||= resource_from_file(file)
+    def animals
+      file = File.join(File.dirname(__FILE__), 'animals.txt')
+      @animals ||= resource_from_file(file)
+    end
   end
 end
