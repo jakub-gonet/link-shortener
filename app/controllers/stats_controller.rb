fd582dd3210a52c87e2de86076af44adfaaf8c8a
Jakub Gonet <jakub.gonet0@gmail.com>
2019-04-01 09:13:38 +0200

    StatsController: Implement index and show

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 class StatsController < ApplicationController
-  def index; end
+  RECENT_DAYS = 30
+
+  def index
+    @urls_views = UrlAccess.count_all_urls_views_since(Date.today - RECENT_DAYS)
+  end
+
+  def show
+    @shortened_url = params[:shortened_url]
+    @url_views = UrlAccess.count_url_views_since(params[:shortened_url],
+                                                 Date.today - RECENT_DAYS)
+  end
 end
