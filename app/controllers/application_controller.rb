32944edf62f4e54c9a5ac0cf81b5fbe97899454a
Jakub Gonet <jakub.gonet0@gmail.com>
2019-04-02 21:16:56 +0200

    RedirectController: Handle exceptions in AppController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,18 @@ class ApplicationController < ActionController::Base
   end
 
   rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid
+  rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+
+  protected
 
   def render_record_invalid(exception)
     flash[:error] = exception.record.errors
     redirect_to root_path, status: :bad_request
+  end
+
+  def render_record_not_found(exception)
+    flash[:error] = exception.to_s
+    logger.info(exception)
+    redirect_to root_path
   end
 end
--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -2,15 +2,9 @@
 
 class RedirectsController < ApplicationController
   def show
-    url = Url.find_by shortened_url: params[:shortened_url]
-    if !url.nil?
-      access = UrlAccess.new(url_id: url.id, ip: request.remote_ip)
-      unless access.save
-        logger.warn("Couldn't save access for ip #{request.remote_ip}, id: #{url.id}")
-      end
-      redirect_to url.base_url
-    else
-      redirect_to root_path, alert: 'Requested URL not found'
-    end
+    url = Url.find_by!(shortened_url: params[:shortened_url])
+
+    UrlAccess.new!(url_id: url.id, ip: request.remote_ip)
+    redirect_to url.base_url
   end
 end
