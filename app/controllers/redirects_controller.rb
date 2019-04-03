1926d997e8b4e97cd51c49914938bf96941c06bf
Jakub Gonet <jakub.gonet0@gmail.com>
2019-04-03 16:34:47 +0200

    RedirectController: Fix wrong method call
    Acidentally used new! instead of create!

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -5,7 +5,7 @@ class RedirectsController < ApplicationController
   def show
     url = Url.find_by!(shortened_url: params[:shortened_url])
 
-    UrlAccess.new!(url_id: url.id, ip: request.remote_ip)
+    UrlAccess.create!(url_id: url.id, ip: request.remote_ip)
     redirect_to url.base_url
   end
 end
